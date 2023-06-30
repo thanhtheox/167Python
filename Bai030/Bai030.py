8ea0a4e8eea0373aa3484e5239725cda6e5d29c8
nguyenhoanganh1808 <1808nguyenhoanganh2003@gmail.com>
2023-06-30 16:02:28 +0700

    sua bai 30

--- Bai030/Bai030.py
+++ Bai030/Bai030.py
@@ -1,7 +1,7 @@
 n = int(input("Nhap n: "))
 s = 0
-for i in range(2, 2 * n + 1, 2):
-    s += 1/i
-print("Tong la: ", s)
-
-
+i = 2
+while i <= 2*n:
+    s = s + 1/i
+    i = i + 2
+print("Tong S = ",s) 
